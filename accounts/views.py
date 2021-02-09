4e1565b20b1023dee6538759949d3879d4534162
Roy Magnussen <roysgamesandmore@gmail.com>
2021-02-09 22:19:04 +0000

    Added email sending when user signs up.

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.core.mail import EmailMessage
 from django.conf import settings
 from django.urls import reverse
 from .forms import AccountSignUpForm
@@ -41,6 +42,7 @@ def payment(request) -> render:
         if form.is_valid():
             form.save()
             request.session['email'] = form.cleaned_data.get('email')
+            request.session['username'] = form.cleaned_data.get('username')
 
             stripe.api_key = settings.STRIPE_SECRET_KEY
             stripe_session = stripe.checkout.Session.create(
@@ -83,5 +85,17 @@ def sign_up_complete(request) -> render:
         'title': 'Sign Up Complete',
         'user_email': request.session['email']
     }
+
+    username = request.session['username']
+    subject = 'Yummy! Account Successfully Created!'
+    email_body = f'Hi {username}!\n Welcome to the Yummy! family! We look forward to seeing you share some wonderful recipes with us.\n\n You are now able to log in.\n\nHappy cooking!\nThe Yummy! team.'
+
+    email = EmailMessage(
+        subject=subject,
+        body=email_body,
+        from_email=settings.EMAIL_HOST_USER,
+        to=[request.session['email']],
+    )
+    email.send(fail_silently=True)
 
     return render(request, 'accounts/signup_complete.html', context)
--- yummy_recipe_blog/settings.py
+++ yummy_recipe_blog/settings.py
@@ -90,6 +90,15 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKENDS = {
+    'django.core.mail.backends.smtp.EmailBackend',
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
