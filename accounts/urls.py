437173f85816ec3cf2dea3297a31faa42c7dbed3
Roy Magnussen <roysgamesandmore@gmail.com>
2021-02-09 17:09:38 +0000

    Created payment and sign up complete urls.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.sign_up, name='sign_up'),
+    path('signup/payment/', views.payment, name='payment'),
+    path('signup/complete/', views.sign_up_complete, name='sign_up_complete'),
 ]
