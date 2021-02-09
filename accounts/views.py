ba78abb0be5521e5f12a1122e7fe3f176f29b881
Roy Magnussen <roysgamesandmore@gmail.com>
2021-02-09 17:07:56 +0000

    Created payment view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.conf import settings
+from django.urls import reverse
 from .forms import AccountSignUpForm
+import stripe
 
 # Create your views here.
 
@@ -21,3 +25,46 @@ def sign_up(request) -> render:
     }
 
     return render(request, 'accounts/signup.html', context)
+
+
+def payment(request) -> render:
+    """
+    Renders the payment view.
+
+    Args:
+        request (HttpRequest): A HttpRequest class object.
+
+    Returns:
+        render: A HttpResponse ojbect whose content is filled by the given template and context.
+    """
+    if request.method == 'POST':
+        form = AccountSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            request.session['email'] = form.cleaned_data.get('email')
+
+            stripe.api_key = settings.STRIPE_SECRET_KEY
+            stripe_session = stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                line_items=[{
+                    'price': 'price_1IG9ejDG4AaONyVC9Xe2Trdy',
+                    'quantity': 1,
+                }],
+                mode='payment',
+                success_url=request.build_absolute_uri(reverse('sign_up_complete')) + '?success_id={CHECKOUT_SESSION_ID}',
+                cancel_url=request.build_absolute_uri(reverse('payment'))
+            )
+
+            context = {
+                'title': 'Payment',
+                'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+                'session_id': stripe_session.id,
+            }
+
+            return render(request, 'accounts/payment.html', context)
+
+        else:
+            return redirect('sign_up')
+
+    elif request.method == 'GET':
+        return redirect('sign_up')
