cc28aba1e38220357cf8f6e796a53321f1975c82
Roy Magnussen <roysgamesandmore@gmail.com>
2021-02-09 00:45:43 +0000

    Created sign up view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .forms import AccountSignUpForm
 
 # Create your views here.
+
+
+def sign_up(request) -> render:
+    """
+    Renders the sign up view.
+
+    Args:
+        request (HttpRequest): A HttpRequest class object.
+
+    Returns:
+        render: A HttpResponse ojbect whose content is filled by the given template and context.
+    """
+    form = AccountSignUpForm()
+    context = {
+        'title': 'Sign Up',
+        'form': form,
+    }
+
+    return render(request, 'accounts/signup.html', context)
