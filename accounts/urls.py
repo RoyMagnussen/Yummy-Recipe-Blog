4523ebfc4fed6052cf773e4a8589674cbe3e6aa6
Roy Magnussen <roysgamesandmore@gmail.com>
2021-02-11 18:25:47 +0000

    Created login url.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login_page, name='login_page'),
     path('signup/', views.sign_up, name='sign_up'),
     path('signup/payment/', views.payment, name='payment'),
     path('signup/complete/', views.sign_up_complete, name='sign_up_complete'),
