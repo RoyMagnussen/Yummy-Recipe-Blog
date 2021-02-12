8ec24bcd04faed742c6f0f46c2d733a6f48fc8e7
Roy Magnussen <roysgamesandmore@gmail.com>
2021-02-12 23:57:03 +0000

    Added tests for reset password views.

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -1,6 +1,8 @@
-from django.http import response
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from .forms import AccountSignUpForm
 
 # Create your tests here.
@@ -32,3 +34,27 @@ class TestViews(TestCase):
         response = self.client.post(
             '/signup/payment/', {'username': 'Test User', 'email': 'test@test.com', 'password1': 'Testing@1234', 'password2': 'Testing@1234'})
         self.assertRedirects(response, '/signup/')
+        
+    def test_get_reset_password_page(self):
+        response = self.client.get('/reset_password/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/reset_password.html')
+        
+    def test_get_reset_password_sent_page(self):
+        response = self.client.get('/reset_password_sent/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/reset_password_sent.html')
+        
+    def test_get_reset_password_change_password_page(self):
+        user = User.objects.create(username='Test User', email='test@test.com', password='Testing@1234')
+        user.save()
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        token = PasswordResetTokenGenerator().make_token(user)
+        response = self.client.get(f'/reset_password/{uid}/{token}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/reset_password_change_password.html')
+        
+    def test_get_reset_password_confirm_page(self):
+        response = self.client.get('/reset_password_confirm/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/reset_password_confirm.html')
