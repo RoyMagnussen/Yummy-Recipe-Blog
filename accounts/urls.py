ce9fcaba55e7fbcd9a465208824944f9cc084d6e
Roy Magnussen <roysgamesandmore@gmail.com>
2021-02-12 20:03:23 +0000

    Added password reset urls.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('signup/payment/', views.payment, name='payment'),
     path('signup/complete/', views.sign_up_complete, name='sign_up_complete'),
     path('reset_password/', views.reset_password, name='reset_password'),
+    path('reset_password_sent/', views.reset_password_sent, name='reset_password_sent'),
+    path('reset_password/<uidb64>/<token>/', views.reset_password_change_password, name='reset_password_send'),
+    path('resest_password_confirm/', views.reset_password_confirm, name='reset_password_confirm'),
 ]
